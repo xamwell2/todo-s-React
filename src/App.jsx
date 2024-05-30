5be18903284025fbe475955901c26c8f5aed1a1e
xamwell2 <bxamwell@gmail.com>
2024-05-30 17:50:58 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from './components/Main';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <Main />
+        </div>
+    );
+};
+
+export default App;
